ed6c11dc179049d5276cd7ff0d7f6ce77d00cc4f
kejbi <biegajski.kacper@gmail.com>
2020-02-15 11:49:45 +0100

    initial commit

---
+++ src/main/java/pl/kejbi/youthresearch/YouthresearchApplication.java
@@ -0,0 +1,13 @@
+package pl.kejbi.youthresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthresearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthresearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kejbi/youthresearch/YouthresearchApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kejbi.youthresearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthresearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
